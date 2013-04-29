6abe9225f7364133f9f13d94fa805c240892eccb
liebekinder <sebast_44@hotmail.fr>
2013-04-29 11:09:04 +0200

    initial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<head>
+	<meta charset="UTF-8"/>
+	<title>semLAV Query Handler</title>
+	<link type="text/css" rel="stylesheet" href="stylesheets/stylesheet.css"/>
+</head>
+
+<body>
+ 
+ <noscript>
+      <div style="width: 22em; position: absolute; left: 50%; margin-left: -11em; color: red; background-color: white; border: 1px solid red; padding: 4px; font-family: sans-serif">
+        Your web browser must have JavaScript enabled
+        in order for this application to display correctly.
+      </div>
+    </noscript>    
+
+    <div id="principal">
+    	<div id="querySelector">
+    	</div>
+    	<div id="queryDisplay">
+    	</div>
+    	<div id="queryResult">
+    		<div id="calculate"></div>
+    		<div id="result"></div>
+    	</div>
+    </div>
+    
+</body>
+
+</html>
